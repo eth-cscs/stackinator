1f5fc79e5155937787cbd22901680bd90b3887fc
bcumming <bcumming@cscs.ch>
2022-10-18 16:44:39 +0200

    building more of the infra

--- lib/sstool/main.py
+++ lib/sstool/main.py
@@ -1,9 +1,13 @@
 import argparse
 import os
 import os.path
+import shutil
 import sys
-import yaml
+
 import jinja2
+import yaml
+
+tool_prefix = ''
 
 def make_argparser():
     parser = argparse.ArgumentParser(description='Generate a build configuration for a spack stack from a recipe.')
@@ -54,26 +58,26 @@ class Recipe:
                 requirement['upstream'] = {'name': R[0], 'spec': R[1]}
             requirements.append(requirement)
 
+        template_path = os.path.join(tool_prefix, 'templates')
         env = jinja2.Environment(
-                loader = jinja2.FileSystemLoader('./templates'),
-                trim_blocks=True,
-                lstrip_blocks=True)
+                loader = jinja2.FileSystemLoader(template_path),
+                trim_blocks=True, lstrip_blocks=True)
 
-        make_template = env.get_template('Makefile.compilers.jinja2')
-
-        files['makefile'] = make_template.render(compilers=self.compilers, requirements=requirements)
+        makefile_template = env.get_template('Makefile.compilers.jinja2')
+        files['makefile'] = makefile_template.render(compilers=self.compilers, requirements=requirements)
 
         # generate compilers/<compiler>/spack.yaml
 
         files['config'] = {}
         for compiler, config in self.compilers.items():
-            make_template = env.get_template('spack.yaml.jinja2')
-            files['config'][compiler] = make_template.render(config=config)
+            spack_yaml_template = env.get_template('spack.yaml.jinja2')
+            files['config'][compiler] = spack_yaml_template.render(config=config)
 
         return files
 
 
 class Build:
+    # The path where the project is to be created
     path = ''
 
     def __init__(self, args):
@@ -91,6 +95,22 @@ class Build:
     def generate(self, recipe):
         os.makedirs(self.path, exist_ok=True)
 
+        template_path = os.path.join(tool_prefix, 'templates')
+        env = jinja2.Environment(
+                loader = jinja2.FileSystemLoader(template_path),
+                trim_blocks=True, lstrip_blocks=True)
+
+        # generate top level makefiles
+        make_user_template = env.get_template('make.user.jinja2')
+        with open(os.path.join(self.path, 'Make.user'), 'w') as f:
+            f.write(make_user_template.render(build_path=self.path, store='/user-environment', verbose=False))
+            f.write('\n')
+            f.close()
+
+        etc_path = os.path.join(tool_prefix, 'etc')
+        for f in ['Makefile', 'Make.inc']:
+            shutil.copy2(os.path.join(etc_path, f), os.path.join(self.path, f))
+
         # generate the makefile and spack.yaml files that describe the compilers
         compilers = recipe.generate_compilers()
         compiler_path = os.path.join(self.path, 'compilers')
@@ -106,10 +126,11 @@ class Build:
                 f.write(yaml)
                 f.close()
 
-def main(argv=None):
+def main(prefix):
+    tool_prefix = prefix
     try:
         parser = make_argparser()
-        args = parser.parse_args(argv)
+        args = parser.parse_args()
         recipe = Recipe(args)
         build = Build(args)
         build.generate(recipe)
