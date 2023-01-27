e3d99b582e433235b4451dca214c2bb3e442fd23
bcumming <bcumming@cscs.ch>
2023-01-27 11:51:27 +0100

    separate compilers schema

--- sstool/schema.py
+++ sstool/schema.py
@@ -1,5 +1,9 @@
+import json
 import jsonschema
+import pathlib
 import yaml
+
+prefix = pathlib.Path(__file__).parent.resolve()
 
 # create a validator that will insert optional fields with their default values
 # if they have not been provided.
@@ -82,58 +86,62 @@ config_schema = {
 }
 
 # for config.yaml files
-compilers_schema = {
-    "type" : "object",
-    "properties" : {
-        "bootstrap" : {
-            "type": "object",
-            "properties": {
-                "spec": {
-                    "type": "string",
-                },
-            },
-            "additionalProperties": False,
-            "required": ["spec"],
-        },
-        "gcc" : {
-            "type": "object",
-            "properties": {
-                "specs": {
-                    "type": "array",
-                    "items": {
-                        "type": "string",
-                    },
-                    "minItems": 1,
-                },
-            },
-            "additionalProperties": False,
-            "required": ["specs"],
-        },
-        "llvm" : {
-            "oneOf": [
-                {
-                    "type": "object",
-                    "properties": {
-                        "requires": "string",
-                        "specs": {
-                            "type": "array",
-                            "items": {
-                                "type": "string",
-                            },
-                            "minItems": 1,
-                        },
-                    },
-                    "additionalProperties": False,
-                    "required": ["requires", "specs"],
-                },
-                {
-                    "type" : "null"
-                },
-            ],
-            "default": None,
-        },
-    },
-    # this restricts to only the fields described above
-    "additionalProperties": False,
-    "required": ["bootstrap", "gcc"]
-}
+compilers_schema = {}
+with open(prefix / 'schema/compilers.json') as f:
+    compilers_schema = json.load(f)
+
+#compilers_schema = {
+#   "type" : "object",
+#   "properties" : {
+#       "bootstrap" : {
+#           "type": "object",
+#           "properties": {
+#               "spec": {
+#                   "type": "string",
+#               },
+#           },
+#           "additionalProperties": False,
+#           "required": ["spec"],
+#       },
+#       "gcc" : {
+#           "type": "object",
+#           "properties": {
+#               "specs": {
+#                   "type": "array",
+#                   "items": {
+#                       "type": "string",
+#                   },
+#                   "minItems": 1,
+#               },
+#           },
+#           "additionalProperties": False,
+#           "required": ["specs"],
+#       },
+#       "llvm" : {
+#           "oneOf": [
+#               {
+#                   "type": "object",
+#                   "properties": {
+#                       "requires": "string",
+#                       "specs": {
+#                           "type": "array",
+#                           "items": {
+#                               "type": "string",
+#                           },
+#                           "minItems": 1,
+#                       },
+#                   },
+#                   "additionalProperties": False,
+#                   "required": ["requires", "specs"],
+#               },
+#               {
+#                   "type" : "null"
+#               },
+#           ],
+#           "default": None,
+#       },
+#   },
+#   # this restricts to only the fields described above
+#   "additionalProperties": False,
+#   "required": ["bootstrap", "gcc"]
+#}
