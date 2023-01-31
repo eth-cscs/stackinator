daad21152baee77a89ab4030cb372b9c43b24ee1
bcumming <bcumming@cscs.ch>
2023-01-31 10:45:12 +0100

    yaml tests for compilers and config done

--- test.py
+++ test.py
@@ -3,10 +3,12 @@
 import pathlib
 import sys
 
-prefix = os.path.abspath(os.path.dirname(__file__))
-external = os.path.join(prefix, 'external')
-sys.path = [prefix, external] + sys.path
+prefix = pathlib.Path(__file__).parent.resolve()
+external = prefix / 'external'
+sys.path = [prefix.as_posix(), external.as_posix()] + sys.path
+
+import pytest
 
 if __name__ == '__main__':
-    sys.argv = [sys.argv[0], 'test']
+    sys.argv = [sys.argv[0], '-vv', 'test']
     sys.exit(pytest.main())
