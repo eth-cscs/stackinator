b9d1c51450845b1b7c0b26cdd47751823f196670
bcumming <bcumming@cscs.ch>
2023-02-13 09:49:16 +0100

    add canonical VERSION file

--- stackinator/__init__.py
+++ stackinator/__init__.py
@@ -1,13 +1,21 @@
 import logging
+import pathlib
 
 
 root_logger = logging.getLogger('stackinator')
 
-#: (major, minor, micro, dev release) tuple
-stackinator_version_info = (0, 0, 1, "dev0")
+#: (major, minor, dev release) tuple
+def get_version():
+    import os
 
-#: PEP440 canonical <major>.<minor>.<micro>.<devN> string
-stackinator_version = ".".join(str(s) for s in stackinator_version_info)
+    parent_path = pathlib.Path(__file__).parent.parent.resolve()
+    with open(parent_path / "VERSION") as version_file:
+        return version_file.read().strip()
+
+#: PEP440 canonical <major>.<minor>.<dev> string
+stackinator_version = get_version()
+
+stackinator_version_info = tuple(stackinator_version.split('.'))
 
 __all__ = ["stackinator_version_info", "stackinator_version"]
 __version__ = stackinator_version
