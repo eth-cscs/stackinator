e939c0529cce5542d6728544f4fd6c8e05cf3457
bcumming <bcumming@cscs.ch>
2024-11-08 16:52:36 +0100

    satisfy black

--- stackinator/builder.py
+++ stackinator/builder.py
@@ -214,14 +214,20 @@ class Builder:
             if capture.returncode != 0:
                 self._logger.debug(f'unable to change to the requested commit {spack["commit"]}')
                 capture.check_returncode()
+
         # the default branch is develop: find the exact commit
-        else:
-            git_commit_result = subprocess.run(["git", "-C", spack_path, "rev-parse", "HEAD"],
-                                               shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if not spack["commit"] or spack["commit"] == "develop":
+            git_commit_result = subprocess.run(
+                ["git", "-C", spack_path, "rev-parse", "HEAD"],
+                shell=False,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
             spack["commit"] = git_commit_result.stdout.strip()
 
         # get the spack commit
         spack_meta = {"commit": spack["commit"], "url": spack["repo"]}
+        print(f"==== {spack_meta}")
 
         # load the jinja templating environment
         template_path = self.root / "templates"
