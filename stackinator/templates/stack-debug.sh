ceeaa1ad12c65026112eb71c1aa1420f33244e2f
Simon Pintarelli <simon.pintarelli@cscs.ch>
2025-03-15 13:27:06 +0100

    fix stack-debug.sh

--- stackinator/templates/stack-debug.sh
+++ stackinator/templates/stack-debug.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+set -eu
+
 env --ignore-environment \
     PATH=/usr/bin:/bin:{{ build_path }}/spack/bin \
     HOME=$HOME BUILD_ROOT={{ build_path }} \
@@ -6,5 +10,5 @@ env --ignore-environment \
     SPACK=spack SPACK_COLOR=always \
     SPACK_USER_CONFIG_PATH={% if spack_version>="0.23" %}~{% else %}/dev/null{% endif %} \
     LC_ALL=en_US.UTF-8 TZ=UTC SOURCE_DATE_EPOCH=315576060 \
-    {% if use_bwrap %} {{ build_path }}/bwrap-mutable-root.sh --tmpfs ~ --bind {{ build_path }}/tmp /tmp -- ${BUILD_ROOT}/bwrap-store.sh {% endif %} \
+    {% if use_bwrap %} {{ build_path }}/bwrap-mutable-root.sh --tmpfs ~ --bind {{ build_path }}/tmp /tmp -- {{ build_path }}/bwrap-store.sh {% endif %} \
     bash -noprofile -l
