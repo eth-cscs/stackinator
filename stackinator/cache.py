855d5f0aec480fae0d9d695434e8e8937d462d6f
Simon Pintarelli <simon.pintarelli@cscs.ch>
2025-03-15 14:54:38 +0100

    Revert "ignore store/mountpath for build cache"
    This reverts commit a13fc2b06e28e2ed9604d657982b97ba505fb1a9.

--- stackinator/cache.py
+++ stackinator/cache.py
@@ -23,8 +23,9 @@ def configuration_from_file(file, mount):
 
         raw["root"] = path
 
-        # Ignore the subdirectory
-        raw["path"] = pathlib.Path(path.as_posix())
+        # Put the build cache in a sub-directory named after the mount point.
+        # This avoids relocation issues.
+        raw["path"] = pathlib.Path(path.as_posix() + mount.as_posix())
 
         # verify that the key file exists if it was specified
         key = raw["key"]
