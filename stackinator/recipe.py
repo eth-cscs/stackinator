8a5375bae1202f3791ceb30733dff33489f2a3f0
bcumming <bcumming@cscs.ch>
2025-06-06 15:53:39 +0200

    add newlnes to version messages

--- stackinator/recipe.py
+++ stackinator/recipe.py
@@ -56,24 +56,21 @@ class Recipe:
         if self.config["version"] != 2:
             rversion = self.config["version"]
             if rversion == 1:
-                self._logger.info(
-                    f"The recipe is an old version 1 recipe for Spack v0.23 and earlier."
-                    "This version of Stackinator supports Spack 1.0, and has deprecated support for Spack v0.23."
-                    "Use version 5 of stackinator, which can be accessed via the releases/v5 branch:"
-                    "    git switch releases/v5"
-                    ""
-                    "If this recipe is to be used with Spack 1.0, then please add the field 'version: 2' to"
-                    "config.yaml in your recipe."
-                    ""
-                    "For more information: https://eth-cscs.github.io/stackinator/recipes/#configuration"
+                self._logger.error(
+                    f"\nThe recipe is an old version 1 recipe for Spack v0.23 and earlier.\n"
+                    "This version of Stackinator supports Spack 1.0, and has deprecated support for Spack v0.23.\n"
+                    "Use version 5 of stackinator, which can be accessed via the releases/v5 branch:\n"
+                    "    git switch releases/v5\n\n"
+                    "If this recipe is to be used with Spack 1.0, then please add the field 'version: 2' to\n"
+                    "config.yaml in your recipe.\n\n"
+                    "For more information: https://eth-cscs.github.io/stackinator/recipes/#configuration\n"
                 )
                 raise RuntimeError("incompatible uenv recipe version")
             else:
-                self._logger.info(
-                    f"The config.yaml file sets an unknown recipe version={rversion}."
-                    "This version of Stackinator supports version 2 recipes."
-                    ""
-                    "For more information: https://eth-cscs.github.io/stackinator/recipes/#configuration"
+                self._logger.error(
+                    f"\nThe config.yaml file sets an unknown recipe version={rversion}.\n"
+                    "This version of Stackinator supports version 2 recipes.\n\n"
+                    "For more information: https://eth-cscs.github.io/stackinator/recipes/#configuration\n"
                 )
                 raise RuntimeError("incompatible uenv recipe version")
 
