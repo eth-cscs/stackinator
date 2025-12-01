893bfba02114c5d4716c71e3f3ce72c8518757dc
Alberto Invernizzi <alberto.invernizzi@cscs.ch>
2025-12-01 18:44:02 +0100

    add back bin/stack config, but in a modernised way
    it was not possible to use uv run --script because we need to know
    where stackinator root is, to add it to the venv using --with

--- stackinator/main.py
+++ stackinator/main.py
@@ -99,3 +99,7 @@ def main():
         root_logger.error(str(e))
         root_logger.info(f"see {logfile} for more information")
         return 1
+
+
+if __name__ == "__main__":
+    sys.exit(main())
