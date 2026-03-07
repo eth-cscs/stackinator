31a16f1d6550a545c625d779afdacc138cfa1e68
grodzki-lanl <grodzki@lanl.gov>
2026-03-07 00:54:59 -0700

    validate keys in mirror config and fixed yaml generator

--- stackinator/mirror.py
+++ stackinator/mirror.py
@@ -2,15 +2,21 @@ import os
 import pathlib
 import urllib.request
 from typing import Optional
+import magic
 
 import yaml
 
 from . import schema
 
+class MirrorConfigError(RuntimeError):
+    """Exception class for errors thrown by mirror configuration problems."""
 
-def configuration_from_file(path: pathlib.Path, cmdline_cache: Optional[str] = None):
+
+
+def configuration_from_file(system_config_root: pathlib.Path, cmdline_cache: Optional[str] = None):
     """Configure mirrors from both the system 'mirror.yaml' file and the command line."""
 
+    path = system_config_root/"mirrors.yaml"
     if path.exists():
         with path.open() as fid:
             # load the raw yaml input
@@ -54,36 +60,33 @@ def configuration_from_file(path: pathlib.Path, cmdline_cache: Optional[str] = N
             # verify that the root path exists
             path = pathlib.Path(os.path.expandvars(url))
             if not path.is_absolute():
-                raise FileNotFoundError(f"The build cache path '{path}' is not absolute")
+                raise FileNotFoundError(f"The mirror path '{path}' is not absolute")
             if not path.is_dir():
-                raise FileNotFoundError(f"The build cache path '{path}' does not exist")
+                raise FileNotFoundError(f"The mirror path '{path}' does not exist")
 
             mirror["url"] = path
 
-        else:
+        elif url.beginswith("https://"):
             try:
                 request = urllib.request.Request(url, method='HEAD')
                 response = urllib.request.urlopen(request)
             except urllib.error.URLError as e:
-                print(f'Error: {e.reason}')
+                raise MirrorConfigError(
+                    f"Could not reach the mirror url '{url}'. " 
+                    f"Check the url listed in mirrors.yaml in system config. \n{e.reason}")
 
-            if mirror["key"]:
-                #if path, check if exists
-                path = pathlib.Path(os.path.expandvars(mirror["key"]))
-                if path.exists():
-                    if not path.is_file():
-                        raise FileNotFoundError(f"The key path '{path}' is not a file")
-                else
-                    #if key, save to file, change to path
-
-            if mirror["bootstrap"]:
-                #make bootstrap dirs
-                #bootstrap/<mirror name>/metadata.yaml
+        if mirror["bootstrap"]:
+            #make bootstrap dirs
+            #bootstrap/<mirror name>/metadata.yaml
 
         return mirrors
 
 
-def generate_mirrors_yaml(mirrors):
+def setup(mirrors, config_path):
+    dst = config_path / "mirrors.yaml"
+    self._logger.debug(f"generate the spack mirrors.yaml: {dst}")
+    with dst.open("w") as fid:
+        fid.write()
     yaml = {"mirrors": {}}
 
     for m in mirrors:
@@ -96,3 +99,36 @@ def generate_mirrors_yaml(mirrors):
         }
 
     return yaml.dump(yaml, default_flow_style=False)
+
+#called from builder
+def key_setup(mirrors: List[Dict], system_config_path: pathlib.Path, key_store: pathlib.Path):
+    for mirror in mirrors:
+        if mirror["key"]:
+            key = mirror["key"]
+            # if path, check if abs path, if not, append sys config path in front and check again
+            path = pathlib.Path(os.path.expandvars(key))
+            if path.exists():
+                if not path.is_absolute():
+                    #try prepending system config path
+                    path = system_config_path + path
+                    if not.path.is_file()
+                        raise FileNotFoundError(
+                            f"The key path '{path}' is not a file. "
+                            f"Check the key listed in mirrors.yaml in system config.")
+                file_type = magic.from_file(path)
+                if not file_type.startswith("OpenPGP Public Key"):
+                    raise MirrorConfigError(
+                        f"'{key}' is not a valid GPG key. "
+                        f"Check the key listed in mirrors.yaml in system config.")
+                # copy file to key store
+                with file open:
+                    data = key.read
+                dest = mkdir(new_key_file)
+                dest.write(data)
+                # mirror["key"] = new_path
+                
+            else:            
+                # if PGP key, convert to binary, ???, convert back
+                # if key, save to file, change to path
+            
+    
